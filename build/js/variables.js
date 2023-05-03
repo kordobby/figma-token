26c2036bb2ddabde3f49b94de82ea5e73c870feb
Yoon Lee <leetrue@chicment.com>
2023-05-04 00:15:10 +0900

    add. test

---
+++ build/js/variables.js
@@ -0,0 +1,15 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 03 May 2023 15:14:50 GMT
+ */
+
+export const Blue1 = "#e6f4ff";
+export const Blue2 = "#bae0ff";
+export const Blue3 = "#91caff";
+export const Blue4 = "#69b1ff";
+export const Blue5 = "#4096ff";
+export const Blue6 = "#1677ff";
+export const Blue7 = "#0958d9";
+export const Blue8 = "#003eb3";
+export const Blue9 = "#002c8c";
+export const Blue10 = "#001d66";
